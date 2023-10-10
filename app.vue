4962bbe39eb0e727ba2fedf11bf39b8ba3116676
Nick <nick.whitt@protonmail.com>
2023-10-10 15:55:43 -0400

    card

--- app.vue
+++ app.vue
@@ -1,5 +1,24 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="container mx-auto p-2">
+    <p>ATB 2019</p>
+    <div class="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-4">
+      <div class="relative flex flex-col rounded border divide-y h-fit">
+        <h3 class="py-2 px-4">American Samoa</h3>
+        <ul class="flex flex-col divide-y h-fit">
+          <li class="flex justify-between py-2 px-4">
+            Avg
+            <ul class="text-center flex gap-1">
+              <li class="border shadow rounded px-2 text-green-700 bg-green-50">
+                P
+              </li>
+              <li class="border shadow rounded px-2">D</li>
+              <li class="border shadow rounded px-2 text-green-700 bg-green-50">
+                W
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
