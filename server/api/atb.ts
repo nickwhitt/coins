4b48dd592e67d813f06e8282b8bdfcc035bd68d9
Nick <nick.whitt@protonmail.com>
2023-10-12 16:14:13 -0400

    server

---
+++ server/api/atb.ts
@@ -0,0 +1,74 @@
+export default defineEventHandler((event) => {
+  return [
+    {
+      date: "2019",
+      reverse: "Lowell NHP",
+      state: "Massachusetts",
+      issues: [
+        {
+          strike: "Avg",
+          mints: [{ mark: "P" }, { mark: "D", have: true }, { mark: "W" }],
+        },
+        { strike: "Unc", mints: [{ mark: "P" }, { mark: "D" }, { mark: "S" }] },
+        { strike: "Pf", mints: [{ mark: "S" }] },
+        { strike: "Pf, Ag", mints: [{ mark: "S" }] },
+      ],
+    },
+    {
+      date: "2019",
+      reverse: "American Memorial Park",
+      state: "Northern Mariana Islands",
+      issues: [
+        {
+          strike: "Avg",
+          mints: [{ mark: "P" }, { mark: "D" }, { mark: "W" }],
+        },
+        { strike: "Unc", mints: [{ mark: "P" }, { mark: "D" }, { mark: "S" }] },
+        { strike: "Pf", mints: [{ mark: "S" }] },
+        { strike: "Pf, Ag", mints: [{ mark: "S" }] },
+      ],
+    },
+    {
+      date: "2019",
+      reverse: "War in the Pacific NHP",
+      state: "Guam",
+      issues: [
+        {
+          strike: "Avg",
+          mints: [{ mark: "P" }, { mark: "D" }, { mark: "W" }],
+        },
+        { strike: "Unc", mints: [{ mark: "P" }, { mark: "D" }, { mark: "S" }] },
+        { strike: "Pf", mints: [{ mark: "S" }] },
+        { strike: "Pf, Ag", mints: [{ mark: "S" }] },
+      ],
+    },
+    {
+      date: "2019",
+      reverse: "San Antonio Missions NHP",
+      state: "Texas",
+      issues: [
+        {
+          strike: "Avg",
+          mints: [{ mark: "P" }, { mark: "D" }, { mark: "W" }],
+        },
+        { strike: "Unc", mints: [{ mark: "P" }, { mark: "D" }, { mark: "S" }] },
+        { strike: "Pf", mints: [{ mark: "S" }] },
+        { strike: "Pf, Ag", mints: [{ mark: "S" }] },
+      ],
+    },
+    {
+      date: "2019",
+      reverse: "Frank Church River of No Return Wilderness",
+      state: "Idaho",
+      issues: [
+        {
+          strike: "Avg",
+          mints: [{ mark: "P" }, { mark: "D" }, { mark: "W" }],
+        },
+        { strike: "Unc", mints: [{ mark: "P" }, { mark: "D" }, { mark: "S" }] },
+        { strike: "Pf", mints: [{ mark: "S" }] },
+        { strike: "Pf, Ag", mints: [{ mark: "S" }] },
+      ],
+    },
+  ];
+});
