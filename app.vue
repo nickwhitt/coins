34de40e014388dc53cb752c018de52868abd9294
Nicholas Whitt <nick.whitt@protonmail.com>
2023-10-15 10:23:01 -0400

    pages

--- app.vue
+++ app.vue
@@ -1,39 +1,7 @@
 <template>
   <div class="container mx-auto p-2">
-    <p>ATB 2019</p>
-    <div
-      class="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4"
-    >
-      <div
-        v-for="issue in atb2019"
-        class="relative flex flex-col rounded shadow-md divide-y h-fit"
-      >
-        <div class="py-2 px-4">
-          <span class="font-semibold">{{ issue.reverse }}</span>
-          <span class="text-sm font-light pl-1">{{ issue.state }}</span>
-        </div>
-        <ul class="flex flex-col divide-y h-fit">
-          <li
-            v-for="issue in issue.issues"
-            class="flex justify-between py-2 px-4"
-          >
-            {{ issue.strike }}
-            <ul class="text-center flex gap-1">
-              <li
-                v-for="mint in issue.mints"
-                class="border shadow rounded px-2 w-7 h-7"
-                :class="{ 'bg-emerald-400': mint.have }"
-              >
-                <span class="text-sm">{{ mint.mark }}</span>
-              </li>
-            </ul>
-          </li>
-        </ul>
-      </div>
-    </div>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
-
-<script setup>
-const { data: atb2019 } = await useFetch("/api/atb");
-</script>
---
+++ pages/index.vue
@@ -0,0 +1,67 @@
+<template>
+  <p>Lincoln, Shield Reverse</p>
+  <div
+    class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4"
+  >
+    <div
+      v-for="x in 10"
+      class="relative flex flex-col rounded shadow-md divide-y h-fit"
+    >
+      <div class="py-2 px-4">
+        <span class="font-semibold">{{ 2024 - x }}</span>
+      </div>
+      <ul class="flex flex-col divide-y h-fit">
+        <li
+          v-for="strike in ['Avg', 'Unc', 'Pf']"
+          class="flex justify-between py-2 px-4"
+        >
+          {{ strike }}
+          <ul class="text-center flex gap-1">
+            <li
+              v-if="strike != 'Pf'"
+              class="border shadow rounded px-2 w-7 h-7"
+            >
+              <span class="text-sm">P</span>
+            </li>
+            <li
+              v-if="strike != 'Pf'"
+              class="border shadow rounded px-2 w-7 h-7"
+            >
+              <span class="text-sm">D</span>
+            </li>
+            <li
+              v-if="x == 5 && strike == 'Unc'"
+              class="border shadow rounded px-2 w-7 h-7"
+            >
+              <span class="text-sm">W</span>
+            </li>
+            <li
+              v-if="strike == 'Pf'"
+              class="border shadow rounded px-2 w-7 h-7"
+            >
+              <span class="text-sm">S</span>
+            </li>
+            <li
+              v-if="x == 5 && strike == 'Pf'"
+              class="border shadow rounded px-2 w-7 h-7"
+            >
+              <span class="text-sm">W</span>
+            </li>
+          </ul>
+        </li>
+        <li v-if="x == 5" class="flex justify-between py-2 px-4">
+          RevPf
+          <ul class="text-center flex gap-1">
+            <li class="border shadow rounded px-2 w-7 h-7">
+              <span class="text-sm">W</span>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const strikes = ["Avg", "Unc", "Pf"];
+</script>
