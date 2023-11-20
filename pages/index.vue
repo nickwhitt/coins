144933b9b839b142d7fcbc92d58fd8da6bcee1d6
Nicholas Whitt <nick.whitt@protonmail.com>
2023-11-20 18:47:07 -0500

    lincoln cents

--- pages/index.vue
+++ pages/index.vue
@@ -1,36 +1,22 @@
+<script setup>
+const { data: cent } = await useFetch("/api/cents/lincoln");
+</script>
+
 <template>
-  <p class="text-2xl my-8">Lincoln, Shield Reverse</p>
-  <div
-    class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4"
-  >
-    <div
-      v-for="shield in data"
-      class="flex flex-col rounded shadow-md divide-y h-fit bg-white"
-    >
-      <div class="py-2 px-4 bg-slate-200">
-        <span class="font-semibold">{{ shield.date }}</span>
+  <div>
+    <div v-for="type in cent.types">
+      <h2>{{ type.type }}</h2>
+      <div
+        class="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 gap-4"
+      >
+        <figure v-for="issue in type.issues">
+          <img class="rounded-full w-[1.9cm] h-[1.9cm] bg-slate-300 mx-auto" />
+          <figcaption class="text-center">
+            <p>{{ issue.issue }}</p>
+            <p class="font-light text-sm">{{ issue.variant }}</p>
+          </figcaption>
+        </figure>
       </div>
-      <ul class="flex flex-col divide-y h-fit">
-        <li
-          v-for="issue in shield.issues"
-          class="flex justify-between py-2 px-4"
-        >
-          {{ issue.strike }}
-          <ul class="text-center flex gap-1">
-            <li
-              v-for="mint in issue.mints"
-              class="border shadow rounded px-2 w-7 h-7"
-              :class="{ 'bg-emerald-400': mint.have }"
-            >
-              <span class="text-sm">{{ mint.mark }}</span>
-            </li>
-          </ul>
-        </li>
-      </ul>
     </div>
   </div>
 </template>
-
-<script setup>
-const { data } = await useFetch("/api/cents/lincoln/shield");
-</script>
